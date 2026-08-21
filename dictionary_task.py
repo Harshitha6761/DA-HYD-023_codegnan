91e5c4e50f6bf16c977696423555e0f7c786f309
Harshitha <harshithaveeragoni@gmail.com>
2026-08-21 11:23:17 +0530

    dictionary task

---
+++ dictionary_task.py
@@ -0,0 +1,15 @@
+
+#task --> user profile using dictionary
+user={'name':'hasrhitha',
+      'id':'CGH049',
+      'batch':'DA-HYD-023',
+      'city': 'JNTU',
+      'email': 'harshithaveeragoni@gmail.com',
+      'phone number':'8328101147',
+      'age':22,
+      'gender':'female',
+      'github':'Harshitha6761'
+      }
+print(user['name'])
+print(user['github'])
+      
